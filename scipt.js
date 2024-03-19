9d11e877487e1b2c1c67f13c2e933c600532aa60
fionicss <118982867+Fionicss@users.noreply.github.com>
2024-03-19 17:15:45 +0800

    Added play object to store players, switchPlayer function, and turn object to invoke gameBoard.

--- scipt.js
+++ scipt.js
@@ -18,7 +18,32 @@ function gameBoard () {
     return {showBoard, addToken}
 }
 
-const test = gameBoard();
-test.addToken(0,0, 'o')
-test.addToken(0,1, 'x')
-console.log (test.showBoard())
+function play (name1, name2) {
+    const player1 = {
+        name: name1,
+        token: "X"
+    };
+    const player2 = {
+        name: name2,
+        token: "O"
+    };
+
+    let currentPlayer = player1;
+    const switchPlayer = () => {
+        currentPlayer = player1 ? currentPlayer = player2 : currentPlayer = player1;
+    }
+
+    const board = gameBoard();
+    const turn = (row, column) => {
+        board.addToken(row, column, currentPlayer.token);
+        console.log(`${currentPlayer.name} places token on row ${row} column ${column}`)
+        console.log(board.showBoard());
+        switchPlayer();
+    }
+
+    return {turn}
+}
+
+const test = play("Patrick", "John");
+test.turn(0,0);
+test.turn(1,1)
