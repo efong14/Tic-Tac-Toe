a1ec80b45bc2a4f7d210b80b215555e2e743da24
fionicss <118982867+Fionicss@users.noreply.github.com>
2024-03-25 18:18:18 +0800

    Added function to check winner in gameBoard to loop over Board to check for winners. Added show winner function in play to display winner based on check winner function.

--- scipt.js
+++ scipt.js
@@ -1,6 +1,7 @@
 function gameBoard () {
     const rows = 3
     const columns = 3
+    winner = ''
     board = []
 
     for (let i = 0; i < rows; i++) {
@@ -11,11 +12,36 @@ function gameBoard () {
     }
 
     const showBoard = () => board;
+    const getWinner = () => winner;
+
     const addToken = (row, column, token) => {
-        board[row][column] = token
+        board[row][column] = token;
     }
 
-    return {showBoard, addToken}
+    const checkWinner = () => {
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] || board[0][2] ==
+            board[1][1] && board[1][1] == board[2][0]) {
+                winner = board[1][1]
+            };
+
+        for (let i = 0; i < 3; i++){
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] == board[i][j+1] && board[i][j+1] == board[i][j+2]){
+                    winner = board[i][j]
+                } return;
+            };
+        };
+
+        for (let j = 0; j < 3; j++){
+            for (let i = 0; i < 3; i++) {
+                if (board[i][j] == board[i][j+1] && board[i][j+1] == board[i][j+2]){
+                    winner = board[i][j]
+                } return;
+            };
+        };
+    };
+
+    return {showBoard, getWinner, addToken, checkWinner}
 }
 
 function play (name1, name2) {
@@ -28,22 +54,35 @@ function play (name1, name2) {
         token: "O"
     };
 
+    const board = gameBoard();
     let currentPlayer = player1;
+
     const switchPlayer = () => {
-        currentPlayer = player1 ? currentPlayer = player2 : currentPlayer = player1;
+        currentPlayer == player1 ? currentPlayer = player2 : currentPlayer = player1;
     }
 
-    const board = gameBoard();
+    const showWinner = () => {
+        board.checkWinner()
+        if (board.getWinner() == '') {
+            switchPlayer();
+            console.log(`It is now ${currentPlayer.name}'s turn`);
+        } else if (board.getWinner !== '') {
+            console.log(`${currentPlayer.name} wins!`)
+        }
+    }
+
     const turn = (row, column) => {
         board.addToken(row, column, currentPlayer.token);
         console.log(`${currentPlayer.name} places token on row ${row} column ${column}`)
         console.log(board.showBoard());
-        switchPlayer();
+        showWinner()
     }
+
+    console.log(`It is now ${currentPlayer.name}'s turn`);
+    console.log(board.showBoard())
 
     return {turn}
 }
 
 const test = play("Patrick", "John");
-test.turn(0,0);
-test.turn(1,1)
+
