cd1786523788e744ad97761bce29c61b222c3eb4
fionicss <118982867+Fionicss@users.noreply.github.com>
2024-04-10 17:39:15 +0800

    Fixed Vertical check, added Tie check, and stopped players from adding their token to a space with a token already.

--- scipt.js
+++ scipt.js
@@ -1,46 +1,58 @@
-const rowOne = document.querySelectorAll('.rowOne')
-const rowTwo = document.querySelectorAll('.rowTwo')
+// Note: Fix tie check
+
+const rowOne = document.querySelectorAll('.rowOne');
+const rowTwo = document.querySelectorAll('.rowTwo');
 const rowThree = document.querySelectorAll('.rowThree')
+;
+
 const gameBoard = (function () {
-    const rows = 3
-    const columns = 3
-    winner = ''
-    board = []
+    const rows = 3;
+    const columns = 3;
+    winner = '';
+    board = [];
 
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
-            board[i].push('')
-        }
-    }
+            board[i].push('');
+        };
+    };
 
     const showBoard = () => board;
-    const showCell = (row, column) => board[row][column]
+    const showCell = (row, column) => board[row][column];
     const getWinner = () => winner;
 
     const addToken = (row, column, token) => {
+        if (board[row][column] != '') {
+            return;
+        };
         board[row][column] = token;
     }
 
     const checkWinner = () => {
+        // Tie check
+        if (!board[0].includes('') && !board[1].includes('') && !board[2].includes('')) {
+            winner = 'Tie!';
+            return;
+        };
+        // Diagonal check
         if (board[0][0] == board[1][1] && board[1][1] == board[2][2] || board[0][2] ==
             board[1][1] && board[1][1] == board[2][0]) {
-                winner = board[1][1]
+                winner = board[1][1];
+                return;
             };
-
+        // Horizontal check
         for (let i = 0; i < 3; i++){
-            for (let j = 0; j < 3; j++) {
-                if (board[i][j] == board[i][j+1] && board[i][j+1] == board[i][j+2]){
-                    winner = board[i][j]
-                } return;
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                    winner = board[i][1];
+                    return;
             };
         };
-
+        // Vertical check
         for (let j = 0; j < 3; j++){
-            for (let i = 0; i < 3; i++) {
-                if (board[i][j] == board[i][j+1] && board[i][j+1] == board[i][j+2]){
-                    winner = board[i][j]
-                } return;
+            if (board[0][j] == board[1][j] && board[1][j] == board[2][j]) {
+                    winner = board[1][j];
+                    return;
             };
         };
     };
@@ -50,25 +62,21 @@ const gameBoard = (function () {
 
 
 const boardDisplay = (function(){
-    let i = 0
-    const displayOne = () => {
+    const displayToken = () => {
+        let i = 0;
+        let j = 0;
+        let k = 0;
         rowOne.forEach(cell => {
-            cell.textContent = `${gameBoard.showCell(0, i++)}`
+            cell.textContent = `${gameBoard.showCell(0, i++)}`;
         });
-    };
-
-    const displayTwo = () => {
         rowTwo.forEach(cell => {
-            cell.textContent = `${gameBoard.showCell(1, i++)}`
+            cell.textContent = `${gameBoard.showCell(1, j++)}`;
         });
-    };
-
-    const displayThree = () => {
         rowThree.forEach(cell => {
-            cell.textContent = `${gameBoard.showCell(0, i++)}`
+            cell.textContent = `${gameBoard.showCell(2, k++)}`;
         });
     };
-    return {displayOne, displayTwo, displayThree}
+    return {displayToken};
 })();
 
 
@@ -86,38 +94,101 @@ function play (name1, name2) {
 
     const switchPlayer = () => {
         currentPlayer == player1 ? currentPlayer = player2 : currentPlayer = player1;
-    }
+    };
 
     const showWinner = () => {
         gameBoard.checkWinner()
         if (gameBoard.getWinner() == '') {
             switchPlayer();
             console.log(`It is now ${currentPlayer.name}'s turn`);
-        } else if (gameBoard.getWinner !== '') {
+        } else if (gameBoard.getWinner() =='Tie!') {
+            console.log(`Tie!`)
+        } else if (gameBoard.getWinner() !== '') {
             console.log(`${currentPlayer.name} wins!`)
-        }
-    }
+        };
+    };
 
     const turn = (row, column) => {
+        if (gameBoard.showCell(row, column) != ''){
+            return;
+        };
         gameBoard.addToken(row, column, currentPlayer.token);
+        boardDisplay.displayToken();
         console.log(`${currentPlayer.name} places token on row ${row} column ${column}`)
         console.log(gameBoard.showBoard());
         showWinner()
-    }
+    };
 
     console.log(`It is now ${currentPlayer.name}'s turn`);
     console.log(gameBoard.showBoard())
     
-    return {turn}
-}
+    return {turn};
+};
 
 
 
 const test = play("Patrick", "John");
-test.turn(0,0)
-test.turn(0,1)
-test.turn(0,2)
-boardDisplay.displayOne();
+
+// WinTest
+// Horizontal P1
+// test.turn(0,2)
+// test.turn(2,0)
+// test.turn(0,1)
+// test.turn(1,0)
+// test.turn(0,0)
+
+// Horizontal P2
+// test.turn(0,2)
+// test.turn(1,0)
+// test.turn(2,1) 
+// test.turn(1,1)
+// test.turn(0,0)
+// test.turn(1,2)
+
+
+// Vertical P1
+// test.turn(0,0)
+// test.turn(0,1)
+// test.turn(1,0)
+// test.turn(2,1)
+// test.turn(2,0)
+
+
+// Vertical P2
+// test.turn(0,2)
+// test.turn(0,0)
+// test.turn(1,1)
+// test.turn(2,0)
+// test.turn(2,2)
+// test.turn(1,0)
+
+// Diagonal p1
+// test.turn(0,0)
+// test.turn(0,1)
+// test.turn(1,1)
+// test.turn(1,0)
+// test.turn(2,2)
+
+// Diagonal p2
+// test.turn (0,2)
+// test.turn(0,0)
+// test.turn(0,1)
+// test.turn(1,1)
+// test.turn(1,0)
+// test.turn(2,2)
+
+// Tie
+// test.turn(0,1)
+// test.turn(0,0)
+// test.turn(1,2)
+// test.turn(1,0)
+// test.turn(1,1)
+// test.turn(0,2)
+// test.turn(2,0)
+// test.turn(2,1)
+// test.turn(2,2)
+
+
 
 
 
